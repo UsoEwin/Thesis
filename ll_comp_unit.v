4612c391b08f02fd6dc32ea1320d1ebbf3788e04
usoewin <insomniacui@hotmail.com>
2018-03-13 15:36:02 -0700

    tb added

--- ll_comp_unit.v
+++ ll_comp_unit.v
@@ -6,15 +6,15 @@
 module ll_comp_unit #(
 	parameter input_width = 32
 )(	
-	input signed [input_width-1:0] din,
+	input signed [input_width:0] din,
 	input en,rst,clk, //rst active high,en active low
-	output wire signed [input_width-1:0] dout,
+  	output wire signed [input_width+1:0] dout, //one bit for sign, one bit for carry
 	output wire data_valid //to the mem block, indicate current data is good
 	);
 	
-	reg signed [input_width-1:0] din_delayed = 0;
-	reg signed [input_width-1:0] dout_mid1 = 0;
-	reg signed [input_width-1:0] dout_mid2 = 0;
+	reg signed [input_width:0] din_delayed = 0;
+  	reg signed [input_width+1:0] dout_mid1 = 0;
+  	reg signed [input_width+1:0] dout_mid2 = 0;
 	always @(posedge clk) begin
 		//sync reset
 		if (rst) begin
@@ -26,16 +26,10 @@ module ll_comp_unit #(
 		else if (~en) begin
 			dout_mid1 <= $signed(din) - $signed(din_delayed); 
 			dout_mid2 <= -($signed(din) - $signed(din_delayed));
-			
 			din_delayed <= din;//from last clk cycle
 		end
 		//if en is high, just hold
-		else begin
-			dout_mid1 <= dout_mid1;
-			dout_mid2 <= dout_mid2;
-			din_delayed <= din_delayed;
-		end
 	end
-	assign dout = (dout_mid1 > 0)? dout_mid1 : dout_mid2; //data can be X
-	assign data_valid = (en === 1'b0);
+ 	assign dout = (dout_mid1 >= 0)? dout_mid1 : dout_mid2; //data can be X
+  assign data_valid = (en == 0);
 endmodule
--- tb_linelength.v
+++ tb_linelength.v
@@ -1,3 +1,5 @@
+// Code your testbench here
+// or browse Examples
 `timescale 1ns/100ps
 
 //this is for unit test of ll_comp_unit
@@ -11,12 +13,13 @@
 `define DATA_WIDTH 32
 module tb_linelength;
 	
-  reg signed [`DATA_WIDTH-1:0] test_din = 0;
+  	reg signed [`DATA_WIDTH-1:0] test_din = 0;
 	reg test_clk = 0;
 	reg test_rst = 0;
 	reg test_en = 0;
   	reg signed [`DATA_WIDTH-1:0] test_din_delay = 0;
-  	wire signed [`DATA_WIDTH-1:0] test_dout = 0; //benchmark value.
+  	wire signed [`DATA_WIDTH:0] test_dout = 0; //benchmark value.
+  	wire test_data_valid = 0;
 //generate clk
 	always #(`CLK_PERIOD/2) test_clk = ~test_clk;
 //instantiate the DUT
@@ -27,27 +30,31 @@ module tb_linelength;
 		.rst(test_rst),
 		.din(test_din),
 		.en(test_en),
-		.dout(test_dout)
+      	.dout(test_dout),
+      	.data_valid(test_data_valid)
 	);
 //test tasks begin here, add more if needed
 //it seems that this trivial implementation won't need task
 
 	initial begin: TB
+        $dumpfile("dump.vcd");
+  		$dumpvars(1);
 		test_din <= 0;
 		test_din_delay <= 0;
 		test_rst <= 0;
-		test_en <= 1;
+		test_en <= 0;
 
-		repeat(5) @ (posedge test_clk);
+      repeat(1) @ (posedge test_clk);
 		test_din <= 1;
 		test_din_delay <= test_din;
-		test_rst <= 1'b1;
-		repeat(1) @ (posedge test_clk);
-      	if (test_dout !== 0) begin
-			
-		end
-		//expecting 0 as output
-
+      repeat(1) @ (posedge test_clk);      
+      	test_din <= 10;
+      	test_din_delay <= test_din;
+      //$display("test_din is = %d", test_din);
+      //$display("test_din_delay is = %d", test_din_delay);
+      repeat(1) @ (posedge test_clk);
+      $display("test_dout is not 0. test_dout = %d", test_dout);
+      repeat(4) @ (posedge test_clk);
 		$finish();
 	end
 endmodule
