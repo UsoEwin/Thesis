167d3ed73903ba42fe8645940fc5321b3fd78951
usoewin <insomniacui@hotmail.com>
2018-03-15 13:59:23 -0700

    accu tested

--- accu_unit.v
+++ accu_unit.v
@@ -1,16 +1,16 @@
 //
-// Accumulator module for our computing 
+// accumulator module for our computing 
 //
-//under testing
+// unit test done
 module accu_unit #(
 	parameter input_width = 37 //computed by 32+log(50) change this for ps and ne
 )(	
 	input signed [input_width-1:0]	din,
 	input en,rst,clk, //rst active high,en active low
   	output wire signed [input_width:0] dout, 
-	output wire 			   data_valid //to the mem block, indicate current data is good
+	output wire 			   data_valid //to the FIFO block, indicate current data is good
 	);
-  	wire signed [input_width-1:0] dout_mid;
+  	reg signed [input_width-1:0] dout_mid;
   	reg [4:0] counter;
 	always @(posedge clk) begin
 		//sync reset
@@ -21,16 +21,13 @@ module accu_unit #(
 
 		else if (~en) begin
 			dout_mid <= $signed(din) + $signed(dout_mid); 
-			if (counter == 49) begin
+          	if (counter == 49) begin
 				counter <= 0;
 			end
-			else begin
-				counter <= counter + 1;
-			end
-			
+			counter <= counter + 1;
 		end
 		//if en is high, just hold
 	end
   	assign dout = dout_mid;
-  	assign data_valid = (counter == 49);
+  assign data_valid = (counter === 49);
 endmodule
---
+++ tb_unit_accu.v
@@ -0,0 +1,55 @@
+`timescale 1ns/100ps
+
+//this is for unit test of accu_unit
+
+//second and ms
+`define SEC 1000000000
+`define MS 1000000
+
+//clk cycle
+`define CLK_PERIOD 30
+`define DATA_WIDTH 37
+module tb_unit_accu;
+	
+  	reg signed [`DATA_WIDTH-1:0] test_din;
+	reg test_clk = 1'b0;
+	reg test_rst;
+	reg test_en;
+  	wire signed [`DATA_WIDTH:0] test_dout; //benchmark value.
+  	wire test_data_valid;
+//generate clk
+	always #(`CLK_PERIOD/2) test_clk = ~test_clk;
+//instantiate the DUT
+	accu_unit #(
+		.input_width(`DATA_WIDTH)
+	) DUT (
+		.clk(test_clk),
+		.rst(test_rst),
+		.din(test_din),
+		.en(test_en),
+      	.dout(test_dout),
+      	.data_valid(test_data_valid)
+	);
+//test tasks begin here, add more if needed
+//it seems that this trivial implementation won't need task
+
+	initial begin: TB
+        $dumpfile("dump.vcd"); //for eda playground wave form
+  		$dumpvars(1);
+		test_din <= 0;
+		test_rst <= 1'b1;
+		test_en <= 0;
+        @(posedge test_clk);
+      
+      	test_rst <= 1'b0;
+
+      repeat(100) begin 
+        @(posedge test_clk);
+		test_din <= $random % 100;
+        $display("data_valid is %d",test_data_valid);
+        $display("din is %d. dout is %d",test_din, test_dout);
+      end
+      repeat(4) @ (posedge test_clk);
+	$finish();
+	end
+endmodule
