cc11d3d8d7d6d6b69e7201f6e127f1bed47cb943
usoewin <insomniacui@hotmail.com>
2018-03-17 11:25:00 -0700

    adder done

---
+++ adder5.v
@@ -0,0 +1,21 @@
+//
+// giant adder to sum up 5 inputs, we don't care the delay
+// you can choose tree adder or whatever
+// under testing
+module adder #(
+	parameter input_width = 37, //for 32 bits input, change this for ne and ll
+	parameter output_width = 40 //for 32 bits input
+)(	
+	input signed [input_width-1:0] din1,
+	input signed [input_width-1:0] din2,
+	input signed [input_width-1:0] din3,
+	input signed [input_width-1:0] din4,
+	input signed [input_width-1:0] din5,
+	//input en,rst,clk, //pure comb logic
+	//input wire data_valid //from the computing unit
+	output signed [output_width-1:0] dout
+	);
+
+	assign dout = din5+din4+din3+din2+din1;
+endmodule
+
