411f1f9b2237c19be92608a8edab42fb49b7ce21
usoewin <insomniacui@hotmail.com>
2018-04-03 16:58:55 -0700

    filter fixed

---
+++ filter_verification/fltr.m
@@ -0,0 +1,19 @@
+clear;
+clc;
+h = fdesign.bandpass('N,F3dB1,F3dB2',6,1,70,250);
+Hd = design(h,'butter');
+fs = 1000;
+t = 0:1/250:1;
+f = 100;
+y = sin(2*pi*f*t);
+yout = filter(Hd,y);
+plot(t,yout);
+save('sin_fs250.mat','y')
+save('sin_yout.mat','yout')
+
+
+%fc = 70;
+%fs = 250;
+
+%[b,a] = butter(6,fc/(fs/2));
+%freqz(b,a)
