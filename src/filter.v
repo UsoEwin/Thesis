d5af3936255f52d0c1658074925a37e05d3207f8
mlee405 <37995309+mlee405@users.noreply.github.com>
2018-04-19 13:50:22 -0700

    4-70Hz IIR Filter with 2^25 scaled coeffs

--- src/filter.v
+++ src/filter.v
@@ -1,6 +1,6 @@
 //`timescale 1ns / 1ps
 
-// Butterworth IIR 6th order 1-70Hz bandpass filter
+// Butterworth IIR 6th order 4-70Hz bandpass filter
 // if you want to modify this refer to:
 // https://github.com/amoudgl/iir-bandstop-filter
 //
@@ -23,45 +23,43 @@ wire signed [63:0] s1_mult1, s1_mult2, s1_mult3, s2_mult1, s2_mult2, s2_mult3, s
 reg signed [63:0] s1_n1, s1_n2, s2_n1, s2_n2, s3_n1, s3_n2; 
 
 // filter coefficients values
-// Given in MATLAB as decimals, multiplied by 10^8 to get integers
-assign a1 = 62391772; // scale value of first section
-assign a2 = -197481425;
-assign a3 = 97544447;
-assign a4 = 62391772; // scale value of second section
-assign a5 = 24596632;
-assign a6 = 34781792;
-assign a7 = 54093177; // scale value of third section 
-assign a8 = -89065915;
-assign a9 = -8186355;
+// Given in MATLAB as decimals, multiplied by 2^25 to get integers
+assign a1 = 20218738; // scale value of first section
+assign a2 = -63709120;
+assign a3 = 30490274;
+assign a4 = 20218738; // scale value of second section
+assign a5 = 7778639;
+assign a6 = 12372378;
+assign a7 = 17515593; // scale value of third section 
+assign a8 = -28399929;
+assign a9 = -1476753;
 
 // update filter variables
-always @ (*)
-begin
     // Section 1
-    s1_mult1 <= a1*x;
-    s1_add1 <= s1_mult1-s1_mult2;
-    s1_add2 <= s1_add1-s1_mult3;
-    s1_add3 <= s1_add2-s1_n2;
-    s1_mult2 <= a2*s1_n1;
-    s1_mult3 <= a3*s1_n2;
+    assign s1_mult1 = a1*x;
+    assign s1_add1 = s1_mult1-s1_mult2;
+    assign s1_add2 = s1_add1-s1_mult3;
+    assign s1_add3 = s1_add2-s1_n2;
+    assign s1_mult2 = a2*s1_n1;
+    assign s1_mult3 = a3*s1_n2;
 
     // Section 2
-    s2_mult1 <= a4*s1_add3;
-    s2_add1 <= s2_mult1-s2_mult2;
-    s2_add2 <= s2_add1-s2_mult3;
-    s2_add3 <= s2_add2-s2_n2;
-    s2_mult2 <= a5*s2_n1;
-    s2_mult3 <= a6*s2_n2;
+    assign s2_mult1 = a4*s1_add3;
+    assign s2_add1 = s2_mult1-s2_mult2;
+    assign s2_add2 = s2_add1-s2_mult3;
+    assign s2_add3 = s2_add2-s2_n2;
+    assign s2_mult2 = a5*s2_n1;
+    assign s2_mult3 = a6*s2_n2;
 
     // Section 3
-    s3_mult1 <= a7*s2_add3;
-    s3_add1 <= s3_mult1-s3_mult2;
-    s3_add2 <= s3_add1-s3_mult3;
-    s3_add3 <= s3_add2-s3_n2;
-    s3_mult2 <= a8*s3_n1;
-    s3_mult3 <= a9*s3_n2;
-
-end 
+    assign s3_mult1 = a7*s2_add3;
+    assign s3_add1 = s3_mult1-s3_mult2;
+    assign s3_add2 = s3_add1-s3_mult3;
+    assign s3_add3 = s3_add2-s3_n2;
+    assign s3_mult2 = a8*s3_n1;
+    assign s3_mult3 = a9*s3_n2;
+  
+  assign y = $signed((s3_mult3) >>> 25);
 
 // Run the filter state machine
 always @ (negedge clk) 
