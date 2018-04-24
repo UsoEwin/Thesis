2ae988f90ebe7aeb0f92420f6eac2a596a4f59de
Chen Fu <chenfu@berkeley.edu>
2018-04-23 20:23:09 -0700

    filter added

--- src/datapath.v
+++ src/datapath.v
@@ -28,34 +28,120 @@ module datapath #(
 );
 	
 //filters
+	wire signed [31:0] filter_4to70_out;
+	filter_4to70 my_filter_4to70(
+		.x(din),
+		.y(filter_4to70_out),
+		.reset(rst),
+		.clk(clk)
+	);
+
 
 //computing module
 
-	//ll
+	// line length
 	wire signed [ll_output_width-1:0] ll_out;
 	wire data_valid_ll;
+
 	ll_module #(input_width,ll_mid_width,ll_output_width) ll(
-
-	.din(din),.en(en),.rst(rst),.clk(clk),.dout(ll_out),.data_valid(data_valid_ll)
-	
+		.din(filter_4to70_out),
+		.en(en),
+		.rst(rst),
+		.clk(clk),
+		.dout(ll_out),
+		.data_valid(data_valid_ll)
 	);
 
-	//ps
-	wire signed [output_width-1:0] ps_out;
-	wire data_valid_ps;
-	ps_module #(input_width,unit_width,mid_width,output_width) ps(
-
-	.din(din),.en(en),.rst(rst),.clk(clk),.dout(ps_out),.data_valid(data_valid_ps)
-	
-	);
-
-	//ne
+	// non-linear energy
 	wire signed [output_width-1:0] ne_out;
 	wire data_valid_ne;
-	ne_module #(input_width,unit_width,mid_width,output_width) ne(
 
-	.din(din),.en(en),.rst(rst),.clk(clk),.dout(ne_out),.data_valid(data_valid_ne)
-	
+	ne_module #(input_width,unit_width,mid_width,output_width) ne(
+		.din(filter_4to70_out),
+		.en(en),
+		.rst(rst),
+		.clk(clk),
+		.dout(ne_out),
+		.data_valid(data_valid_ne)
+	);
+
+	// power spectrum 4-70
+	wire signed [output_width-1:0] ps_out;
+	wire data_valid_ps;
+
+	ps_module #(input_width,unit_width,mid_width,output_width) ps(
+		.din(filter_4to70_out),
+		.en(en),
+		.rst(rst),
+		.clk(clk),
+		.dout(ps_out),
+		.data_valid(data_valid_ps)
+	);
+
+	// theta
+	wire signed [31:0] theta_out;
+	f_4to8 theta_band(
+		.clk(clk),
+		.reset(rst),
+		.x(filter_4to70_out),
+		.y(theta_out)
+	);
+
+	// alpha
+	wire signed [31:0] alpha_out;
+	f_8to14 alpha_band(
+		.clk(clk),
+		.reset(rst),
+		.x(filter_4to70_out),
+		.y(alpha_out)
+	);
+
+	// beta
+	wire signed [31:0] beta_out;
+	f_14to32 beta_band(
+		.clk(clk),
+		.reset(rst),
+		.x(filter_4to70_out),
+		.y(beta_out)
+	);
+
+	// power spectrum theta band
+	wire signed [output_width-1:0] ps_out_theta;
+	wire data_valid_ps_theta;
+
+	ps_module #(input_width,unit_width,mid_width,output_width) ps_theta(
+		.din(theta_out),
+		.en(en),
+		.rst(rst),
+		.clk(clk),
+		.dout(ps_out_theta),
+		.data_valid(data_valid_ps_theta)
+	);
+
+	// power spectrum alpha band
+	wire signed [output_width-1:0] ps_out_alpha;
+	wire data_valid_ps_alpha;
+
+	ps_module #(input_width,unit_width,mid_width,output_width) ps_alpha(
+		.din(alpha_out),
+		.en(en),
+		.rst(rst),
+		.clk(clk),
+		.dout(ps_out_alpha),
+		.data_valid(data_valid_ps_alpha)
+	);
+
+	// power spectrum beta band
+	wire signed [output_width-1:0] ps_out_beta;
+	wire data_valid_ps_beta;
+
+	ps_module #(input_width,unit_width,mid_width,output_width) ps_beta(
+		.din(beta_out),
+		.en(en),
+		.rst(rst),
+		.clk(clk),
+		.dout(ps_out_beta),
+		.data_valid(data_valid_ps_beta)
 	);
 
 //controller
---
+++ src/filter_8to14_alpha.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+module f_8to14(clk, reset, x, y) ;
+
+output wire signed [31:0] y;
+input wire signed [31:0] x;
+
+// filter coefficients
+wire signed [31:0] b1, b2, b3,
+                   a2, a3;
+input wire clk, reset ;
+
+// filter variables
+wire signed [63:0] b1_in, b2_in, b3_in;
+wire signed [63:0] a2_out, a3_out, a4_out, a5_out, a6_out, a7_out, a8_out, a9_out, a10_out, a11_out, a12_out, a13_out;
+
+// history pipeline regs
+reg signed [63:0] f1_n1, f1_n2;
+ 
+// history pipeline input
+wire signed [63:0] f1_n1_input, f1_n2_input, f1_n0; 
+
+// filter coefficients values
+assign a2 = -240788605;
+assign a3 = 115363971;
+  
+assign b1 = 9426879;
+assign b2 = 0;
+assign b3 = -9426879;
+
+// update filter variables
+assign b1_in = b1*x;
+assign b2_in = b2*x;
+assign b3_in = b3*x;
+
+assign a2_out = a2*f1_n0;
+assign a3_out = a3*f1_n0;
+
+// add operations
+assign f1_n1_input = b2_in + f1_n2 - a2_out;
+assign f1_n2_input = b3_in - a3_out;
+
+// scale the output and turncate for audio codec
+  assign f1_n0 = $signed((f1_n1 + b1_in) >>> 27);
+  assign y = f1_n0;
+
+// Run the filter state machine at audio sample rate
+always @ (negedge clk) 
+begin
+    if (reset)
+    begin
+        f1_n1 <= 0;
+        f1_n2 <= 0;            
+    end
+    else 
+    begin
+        f1_n1 <= f1_n1_input;
+        f1_n2 <= f1_n2_input;            
+    end
+end 
+endmodule
